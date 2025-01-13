6f5468a5505c26cea8287ec5036dc5e51002987a
luis beqja <luisbeqjamw@gmail.com>
2025-01-13 11:58:34 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //System.out.println(new LetterCombinationsPhoneNumber17().letterCombinations("23"));
+        //System.out.println( new GenerateParentheses22().generateParenthesis(3));
+    }
+}
